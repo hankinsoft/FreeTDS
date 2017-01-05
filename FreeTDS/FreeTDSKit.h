d652af47967985dce503a35102523339a647250c
Kyle Hankinson <kylehankinson@Kyles-MacBook-Pro.local>
2017-01-05 17:49:18 -0400

    + Module umbrella header.

---
+++ FreeTDS/FreeTDSKit.h
@@ -0,0 +1,15 @@
+//
+//  FreeTDSKit.h
+//  FreeTDSKit
+//
+//  Created by Kyle Hankinson on 2017-01-05.
+//
+//
+
+//! Project version number for FreeTDSKit.
+FOUNDATION_EXPORT double FreeTDSKitVersionNumber;
+
+//! Project version string for FreeTDSKit.
+FOUNDATION_EXPORT const unsigned char FreeTDSKitVersionString[];
+
+#import "sybdb.h"
